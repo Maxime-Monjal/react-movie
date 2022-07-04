c1d364c49e4ad813376b60d821d9ad33a0f241a1
Maxime-Monjal <maxime.monjal@gmail.com>
2022-07-04 12:44:58 +0200

    change a to link react-router-dom

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,37 +1,39 @@
+import { Link } from "react-router-dom";
+
 export const NavBar = () => {
   return (
     <>
       <div>
         <ul className="flex max-w-screen-xl mx-auto">
           <li className="flex-1 mr-2 ml-4">
-            <a
-              href="/"
+            <Link
+              to="/"
               className="w-full text-center block rounded py-2 px-4 text-blue-500"
             >
               <span className="max-w-screen-xl md:text-4xl pt-8 sm:pl-4">
                 Films du moment
               </span>
-            </a>
+            </Link>
           </li>
           <li className="flex-1 mr-2">
-            <a
-              href="/top_rated"
+            <Link
+              to="/top_rated"
               className="w-full text-center block rounded py-2 px-4 text-blue-500"
             >
               <span className="max-w-screen-xl md:text-4xl pt-8 sm:pl-4">
                 Films les mieux notés
               </span>
-            </a>
+            </Link>
           </li>
           <li className="flex-1 mr-2">
-            <a
-              href="/upcoming"
+            <Link
+              to="/upcoming"
               className="w-full text-center block rounded py-2 px-4 text-blue-500"
             >
               <span className="max-w-screen-xl md:text-4xl pt-8 sm:pl-4">
                 Films à venir
               </span>
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
